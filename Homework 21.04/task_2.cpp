aa5b2b25d2bd7b3bd0561d87274406ffb009107c
hxxdraised <murmif02@gmail.com>
2021-04-22 01:14:02 +0300

    task 2

---
+++ Homework 21.04/task_2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int binarySearch(int a[], int fromIndex, int toIndex, int key, bool last)
+{
+    int low = fromIndex;
+    int high = toIndex - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) >> 1;
+        int midVal = a[mid];
+
+        if (midVal < key || (last && midVal == key))
+            low = mid + 1;
+        else if (midVal > key || (!last && midVal == key))
+            high = mid - 1;
+    }
+    return last ? high : low;
+}
+
+int main()
+{
+    int n, m;
+    int key;
+    int index;
+    int indexl;
+
+    cout << "N: ";
+    cin >> n;
+    cout << "M: ";
+    cin >> m;
+
+    int array[n * m];
+    cout << "Write matrix:\n";
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            cin >> array[i * n + j];
+
+    for (int i = 0; i < n * m; i++)
+        cout << array[i] << " | ";
+    cout << "size: " << n * m;
+    cout << "\nWrite number: ";
+    cin >> key;
+
+    index = binarySearch(array, 0, n * m, key, false);
+    index = (array[index] == key) ? index : -1;
+
+    if (index >= 0)
+    {
+        cout << "Number exist in matrix(index: " << index << ")" << endl;
+    }
+    else
+        cout << "No such number in matrix" << endl;
+
+    return 0;
+}
